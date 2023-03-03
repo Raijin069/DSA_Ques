8af3aee9791cfed7766090f4323fdd93371978d2
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-03-03 19:35:33 +0530

    count 1 bits aur non reapeating char ka ques kiya hai

---
+++ non_repeating_char.py
@@ -0,0 +1,18 @@
+def non_repeating_char(arr: list,n: int) -> list:
+    size= 2*(n+1)
+    count_dict = {arr[i]:0 for i in range(size)}
+    for i in range(size):
+        count_dict[arr[i]] += 1
+    ans_arr = [key for key,value in count_dict.items() if value==1 ]
+    ans_arr.sort()
+    return ans_arr
+
+n=2
+arr=[1, 2, 3, 2, 1, 4]
+
+n=1
+arr=[1, 2, 3, 2, 1, 4]
+
+print(*non_repeating_char(arr,n))
+
+            
