a18a6c609da424417e7447adf6086ce81ba6bb67
Replit user <>
2023-06-28 07:25:08 +0000

    roman number ke ques kiye hai

---
+++ int_to_roman.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        ans = ""
+
+        i_to_r = {1000:"M",900:"CM",500:"D",\
+        400:"CD",100:"C",90:"XC",50:"L",40:"XL",\
+        10:"X",9:"IX",5:"V",4:"IV",1:"I"}
+        
+        for i in i_to_r:
+            
+            while num>= i:
+                num-=i
+                ans+=i_to_r[i]
+        
+        return ans
