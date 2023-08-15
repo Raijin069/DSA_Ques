f092262f7fd1a5617018d53daba4a6c1983a6e23
Replit user <>
2023-08-15 10:23:49 +0000

    ll ke 3 ques aur string aur binary search ka ek ques kiya hai

--- multiply_ll.py
+++ multiply_ll.py
@@ -31,6 +31,16 @@ def multiplyTwoList(head1, head2):
         
     return num1*num2%MOD
 
+def multiplyTwoList(head1, head2):
+    num1,num2=0,0
+    while head1 is not None:
+        num1=10*num1+head1.data
+        head1 = head1.next
+    while head2 is not None:
+        num2=10*num2+head2.data
+        head2 = head2.next
+    return (num1*num2)%MOD
+
 
 
 
--- remove_nth_node_ll.py
+++ remove_nth_node_ll.py
@@ -13,3 +13,24 @@ class Solution:
         
         slow.next = slow.next.next 
         return head 
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast = head  
+        slow = head
+
+        len_of_ll = 0
+        while fast is not None:
+            len_of_ll+=1
+            fast = fast.next
+
+        if len_of_ll == n:
+            head=head.next
+            return head
+
+        for i in range(len_of_ll-n-1):
+            slow=slow.next
+         
+        slow.next = slow.next.next
+        return head
